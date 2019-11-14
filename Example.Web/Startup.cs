33a9eeeb01bff662aab57c41a162b4476ba47a58
John Callaway <john@1north.com>
2019-11-14 14:19:23 -0500

    Update to .NET Core 3.0

--- Example.Web/Startup.cs
+++ Example.Web/Startup.cs
@@ -8,10 +8,10 @@ using Example.Web.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
 
@@ -48,24 +48,16 @@ namespace Example.Web
                 .WriteTo.Debug()
                 .CreateLogger();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped(typeof(IMapper<Dto.Horse, Models.HorseSummary>), typeof(HorseToHorseSummaryMapper));
             services.AddScoped(typeof(IMapper<Dto.Horse, Models.HorseDetail>), typeof(HorseToHorseDetailMapper));
             services.AddTransient<IHorseService, HorseService>();
-        }
 
-        private ColumnOptions GetColumnOptions()
-        {
-            return new ColumnOptions
-            {
-                TimeStamp = { ConvertToUtc = true }
-            };
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -74,19 +66,30 @@ namespace Example.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseDeveloperExceptionPage();
+            app.UseRouting();
 
-            app.UseMvc(routes =>
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private ColumnOptions GetColumnOptions()
+        {
+            return new ColumnOptions
+            {
+                TimeStamp = { ConvertToUtc = true }
+            };
         }
     }
 }
